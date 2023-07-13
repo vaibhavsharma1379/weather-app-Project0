9e60bd41775fa38f54f63a23314e4d6515dfadeb
vaibhavsrh78@gmail.com <vaibhavsrh78@gmail.com>
2023-07-13 10:48:31 +0530

    first commit

---
+++ usercntrl.js
@@ -0,0 +1,318 @@
+var app = angular.module("app", ['ngRoute']);
+app.config(function ($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "login.html"
+    }).when("/dashboard", {
+        templateUrl: "dashboard.html",
+        controller:"currentwther"
+    }).when("/ragister", {
+        templateUrl: "ragister.html"
+    }).when("/facts", {
+        templateUrl: "facts.html"
+    }).when("/sports", {
+        templateUrl: "sports.html"
+    }).when("/astronomy", {
+        templateUrl: "astronomy.html"
+    }).otherwise({
+        redirectTo: '/'
+    });
+});
+
+app.controller("loginctrl", ["$scope", "$location", "loginService", function (p, $location, loginService) {
+    p.redirect = function () {
+        $location.path('/dashboard')
+    }
+    p.userarr;
+    p.loginsuccess = false;
+    p.dispplayPswIncMsg = false;
+    p.displayUsrNtFnd = false;
+    p.Isshow = function () {
+        p.isShow = false;
+
+    }
+    // p.print=function(){
+    // //    p.userarr.push(user);
+    // var user = {
+    //     email: p.loginemail,
+    //     password: p.loginpassword
+    // }
+    //    console.log(user);
+    // }
+
+    p.login = function () {
+        loginService.loginUser(
+            function (usersdata) {
+                p.userarr = usersdata;
+                let flag = false;
+                for (user of p.userarr) {
+                    if (user.email === p.loginemail && user.password === p.loginpassword) {
+                        p.loginsuccess = true;
+                        console.log("loginsucess")
+                        flag = true;
+                        p.redirect();
+                        break;
+                    }
+                    else if (user.email === p.loginemail && user.password !== p.loginpassword) {
+                        p.dispplayPswIncMsg = true;
+                        alert("incorrect paassword")
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag == false) {
+                    alert("User Not found Please Ragister")
+                }
+            }
+        );
+    }
+}])
+
+app.controller("rgtrCtrl", ["$scope", "$location", "rgtrService", function (p, $location, rgtrService) {
+
+    p.isSHow = function () {
+        p.isShow = false;
+
+    }
+    p.ragister = function () {
+        var userData = {
+            name: p.name,
+            email: p.email,
+            password: p.password
+        }
+        p.redirect = function () {
+            $location.path('/dashboard')
+        }
+        if (p.password === p.confirmPassword) {
+            rgtrService.checkUserPresence(function (users) {
+                var userPrsence = false;
+                for (user of users) {
+                    // console.log(user)
+                    if (user.email == p.email) {
+                        userPrsence = true;
+                        console.log(user)
+                        alert("user already exist! Please Login")
+                        break;
+                    }
+                }
+                if (!userPrsence) {
+                    rgtrService.ragisterUser(userData);
+                    p.redirect()
+                }
+            })
+        }
+        else {
+            alert("password not matched")
+        }
+
+
+        // else{
+        //     alert("user already exist! Please Login")
+        // }
+    }
+
+}])
+
+app.controller("currentwther", ["$scope", "$interval", "currentweatherservice", function (p, $interval, currentweatherservice) {
+    p.name;
+    p.region;
+    p.country;
+    p.temp_c;
+    p.text;
+    p.feelslike_c;
+    p.img;
+    var src_base = `./64x64/`
+    p.is_day;
+    p.imglink;
+    p.date = new Date();
+    
+
+
+
+    p.cityName;
+    p.searchWeather = function (cityname) {
+        // alert("searching");
+        currentweatherservice.currentWthr("/current.json", cityname, null, function (res) {
+            var wind_mph = res.current.wind_mph;
+            var wind_kph = res.current.wind_mph;
+            var precip_mm = res.current.precip_mm;
+            var precip_in = res.current.precip_in;
+            var cloud = res.current.cloud;
+            var visibility = res.current.vis_miles;
+
+            console.log(res)
+
+            if (wind_mph > 35 || wind_kph > 56) {
+                p.imglink = "url('https://mdbgo.io/ascensus/mdb-advanced/img/thunderstorm.gif')";
+            } else if (precip_mm > 0.1 || precip_in > 0.25) {
+                if (visibility < 1) {
+                    p.imglink = "url('https://mdbgo.io/ascensus/mdb-advanced/img/fog.gif')";
+                } else {
+                    p.imglink = "url('https://mdbgo.io/ascensus/mdb-advanced/img/rain.gif')";
+                }
+            } else if (cloud > 75) {
+                p.imglink = "url('https://mdbgo.io/ascensus/mdb-advanced/img/clouds.gif')";
+            } else if (cloud > 50) {
+                p.imglink = "url('https://mdbgo.io/ascensus/mdb-advanced/img/clouds.gif')";
+            } else if (visibility < 1) {
+                p.imglink = "url('https://mdbgo.io/ascensus/mdb-advanced/img/fog.gif')";
+            } else {
+                p.imglink = "url('https://mdbgo.io/ascensus/mdb-advanced/img/clear.gif')";
+            }
+            p.currback = {
+                "background-image": p.imglink
+            };
+            // console.log(res)
+            p.name = res.location.name;
+            p.region = res.location.region;
+            p.country = res.location.country;
+            p.temp_c = res.current.temp_c;
+            p.text = res.current.condition.text;
+            p.feelslike_c = res.current.feelslike_c;
+            var iconl = res.current.condition.icon;
+            p.img = `${src_base}${iconl.substring(iconl.length - 11)}`
+        });
+    }
+    p.searchWeather(null);
+    $interval(function () {
+        p.date = new Date();
+        
+    }, 1000);
+    $interval(function () {
+        p.searchWeather(null);
+        
+    }, 100000);
+}])
+
+app.controller("hourlyWeather", ["$scope", "$interval", "currentweatherservice", function (p, $interval ,currentweatherservice) {
+    p.hourlyWeatherData = [];
+    p.icon;
+    currentweatherservice.currentWthr("/forecast.json", null, null, function (res) {
+        p.hourlyWeatherData = res.forecast.forecastday[0].hour;
+        // console.log(res);
+
+
+        // console.log(p.hourlyWeatherData);
+
+    })
+}])
+
+app.controller("daillyweather", ["$scope", "currentweatherservice", function (p, currentweatherservice) {
+    p.daiilyData = [];
+    var startDate = new Date();
+    var endDate = new Date();
+    endDate.setDate(startDate.getDate() + 14);
+    var currentDate = startDate;
+    var numDays = 0;
+    while (currentDate <= endDate) {
+        var DateString = currentDate.toISOString().slice(0, 10);
+        console.log(DateString);
+
+        currentweatherservice.currentWthr("/forecast.json", null, DateString, function (res) {
+            p.daiilyData.push([res.forecast.forecastday[0].day, res.forecast.forecastday[0].date]);
+            numDays++;
+            if (numDays === 15) {
+                p.daiilyData.sort(function (a, b) {
+                    var dateA = new Date(a[1]);
+                    var dateB = new Date(b[1]);
+                    return dateA - dateB;
+                });
+                console.log(p.daiilyData);
+            }
+
+        })
+        currentDate.setDate(currentDate.getDate() + 1);
+    }
+
+
+
+}])
+
+app.controller("astronomy",["$scope","$interval","currentweatherservice",function(s,$interval,currentweatherservice){
+    s.astronomyarr;
+    s.backgroungimg;
+    s.moonphase;
+    s.myImage;
+   var wea=function(){
+    currentweatherservice.currentWthr("/astronomy.json",null,null,function(res){
+        s.astronomyarr=res;
+        
+        if(s.astronomyarr.astronomy.astro.is_sun_up==1){
+            s.backgroungimg={
+                "background-image": 'url("./64x64/astronomy/day.jpg")',
+            }
+            s.textcolor={
+                "color":"black"
+            }
+            
+        }
+        else{
+            s.backgroungimg={
+                "background-image": 'url("./64x64/astronomy/night.jpg")',
+                
+            }
+            s.textcolor={
+                "color":"white"
+            }
+
+        }
+        if (s.astronomyarr.astronomy.astro.moon_phase === 'New Moon') {
+            s.myImage = './64x64/astronomy/new-moon.jpg';
+        } else if (s.astronomyarr.astronomy.astro.moon_phase === 'Waxing Crescent') {
+            s.myImage = './64x64/astronomy/waxing-crescent-moon.jpg';
+        } else if (s.astronomyarr.astronomy.astro.moon_phase === 'First Quarter') {
+            s.myImage = './64x64/astronomy/first-quarter-moon.jpg';
+        } else if (s.astronomyarr.astronomy.astro.moon_phase === 'Waxing Gibbous') {
+            s.myImage = './64x64/astronomy/waxing-gibbous-moon.jpg';
+        } else if (s.astronomyarr.astronomy.astro.moon_phase=== 'Full Moon') {
+           s.myImage = './64x64/astronomy/full-moon.jpg';
+        } else if (s.astronomyarr.astronomy.astro.moon_phase === 'Waning Gibbous') {
+           s.myImage = './64x64/astronomy/waning-gibbous-moon.jpg';
+        } else if (s.astronomyarr.astronomy.astro.moon_phase === 'Last Quarter') {
+            s.myImage = './64x64/astronomy/thisrd-quarter-moon.jpg';
+        } else if (s.astronomyarr.astronomy.astro.moon_phase === 'Waning Crescent') {
+            s.myImage = './64x64/astronomy/waning-gibbous-moon.jpg';
+        }
+
+
+
+        console.log("erer");
+    })
+   }
+   wea();
+    $interval(function () {
+        wea();
+        
+    }, 12000);
+}])
+
+app.controller("sportscntrl",["$scope","currentweatherservice",function(s,currentweatherservice){
+    s.football;
+    s.cricket;
+    s.golf;
+    s.matchs;
+    currentweatherservice.currentWthr("/sports.json",null,null,function(res){
+        s.football=res.football
+        s.cricket=res.cricket;
+        s.golf=res.golf;
+        s.matchs={
+            football:s.football,
+            cricket:s.cricket,
+            golf:s.golf
+        }
+        console.log(s.matchs);
+        
+    })
+    
+   
+}])
+
+
+
+app.controller("factscntrlr",["$scope","currentweatherservice", function(s,currentweatherservice){
+    s.factslist;
+    currentweatherservice.uniqueFacts(function(res){
+        // console.log(res[0].fact)
+        s.factslist=res;
+    })
+}])
+
---
+++ userservice.js
@@ -0,0 +1,82 @@
+app.service("loginService",["$http",function(h){
+    this.loginUser=function(ulogin){
+        h({
+            method:"GET",
+            url:" http://localhost:3000/users"
+        }).then(function(response){
+            ulogin(response.data);
+        },function(e){
+            console.log(e)
+        })
+    }
+}])
+
+app.service("rgtrService",["$http",function(h){
+    this.checkUserPresence=function(checkUser){
+        h({
+           method:"GET",
+           url:"http://localhost:3000/users" 
+        }).then(function(res){
+                checkUser(res.data);
+        },function(e){
+            console.log(e);
+            alert(e);
+        })
+    }
+    this.ragisterUser=function(user){
+        console.log(user);
+        h({
+            method:"POST",
+            url:"http://localhost:3000/users",
+            data:user
+        }).then(function(response){
+            console.log(response);
+            alert(`${response.data.email} is Ragistered`);
+        },function(e){
+            console.log(e);
+        })
+    }
+}])
+
+app.service("currentweatherservice", ["$http", function (h) {
+    var base_url = `http://api.weatherapi.com/v1`;
+  var api_key = "93521c6e22c244cbbe944012230807";
+  var factsApiBaseUrl = `https://api.api-ninjas.com/v1`;
+  var factsApiKey = "yiT8fq31WUwYkk1Q7j+RsQ==auUCm2ZHCKxOvt5E"; // Replace with your own API key
+  
+  this.uniqueFacts = function(cb) {
+    h({
+      method: "GET",
+      url: `${factsApiBaseUrl}/facts?limit=10`,
+      headers: {
+        'X-Api-Key': factsApiKey
+      }
+    }).then(function(res) {
+      cb(res.data);
+    }, function(e) {
+      console.log(e);
+    });
+  }
+
+    this.currentWthr = function (datatype,cityName,date,cb) {
+        
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var latitude =position.coords.latitude;
+                var longitude =position.coords.longitude;
+                var searchpara=`${latitude},${longitude}`
+                console.log(latitude, longitude);
+                h({
+                    method: "GET",
+                    url: `${base_url}${datatype}?key=${api_key}&q=${cityName!==null?cityName:searchpara}&dt=${date}`
+                }).then(function (res) {
+                    cb(res.data);
+                }, function (e) {
+                    console.log(e);
+                })
+            });
+        }
+
+        
+    }
+}])
